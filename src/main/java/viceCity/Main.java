d315b56cef5775320c15f8b3d39bf1cd34240fa6
randered <68559794+randered@users.noreply.github.com>
2021-06-17 20:57:25 +0300

    Created BaseGun Class

--- src/main/java/viceCity/Main.java
+++ src/main/java/viceCity/Main.java
@@ -4,10 +4,13 @@ import viceCity.core.EngineImpl;
 import viceCity.core.interfaces.Controller;
 import viceCity.core.interfaces.Engine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Controller controller = null; // TODO
         Engine engine = new EngineImpl(controller);
         engine.run();
+
     }
 }
---
+++ src/main/java/viceCity/models/guns/BaseGun.java
@@ -0,0 +1,67 @@
+package viceCity.models.guns;
+
+import viceCity.common.ExceptionMessages;
+
+public abstract class BaseGun implements Gun {
+    private String name;
+    private int bulletsPerBarrel;
+    private int totalBullets;
+    private boolean canFire;
+
+    public BaseGun(String name, int bulletsPerBarrel, int totalBullets) {
+        setName(name);
+        setBulletsPerBarrel(bulletsPerBarrel);
+        setTotalBullets(totalBullets);
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty() || name.isBlank()) {
+            throw new NullPointerException
+                    (ExceptionMessages.NAME_NULL);
+        }
+        this.name = name;
+    }
+
+    public void setBulletsPerBarrel(int bulletsPerBarrel) {
+        if (bulletsPerBarrel >= 0) {
+            this.bulletsPerBarrel = bulletsPerBarrel;
+        }
+        throw new IllegalArgumentException
+                (ExceptionMessages.BULLETS_LESS_THAN_ZERO);
+    }
+
+    public void setTotalBullets(int totalBullets) {
+        if (totalBullets >= 0) {
+            this.totalBullets = totalBullets;
+        }
+        throw new IllegalArgumentException
+                (ExceptionMessages.TOTAL_BULLETS_LESS_THAN_ZERO);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBulletsPerBarrel() {
+        return this.bulletsPerBarrel;
+    }
+
+
+    @Override
+    public int getTotalBullets() {
+        return this.totalBullets;
+    }
+
+    @Override
+    public int fire() {
+        return 0;
+
+    }
+
+    @Override
+    public boolean canFire() {
+        return false;
+    }
+}
